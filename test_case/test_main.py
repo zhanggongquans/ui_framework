dec5328cbd025d897346a5632588b2b48c1493d7
zhanggongquans <1152638495@qq.com>
2020-05-31 14:04:18 +0800

    测试数据驱动

--- test_case/test_main.py
+++ test_case/test_main.py
@@ -1,14 +1,16 @@
-
-
 import pytest
+import yaml
 
 from page.app import App
 
 
 class TestMain:
-    def test_main(self):
-        app = App()
-        app.start().main().goto_search()
+    @pytest.mark.parametrize("value1,value2", yaml.safe_load(open("../test_case/test_main.yml")))
+    def test_main(self, value1, value2):
+        # app = App()
+        # app.start().main().goto_search()
+        print(value1)
+        print(value2)
 
 
 if __name__ == '__main__':
