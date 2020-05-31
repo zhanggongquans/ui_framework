5feaa31c75702980a0ec88824691a23446e0d54b
zhanggongquans <1152638495@qq.com>
2020-05-31 14:12:57 +0800

    测试数据驱动

--- test_case/test_main.py
+++ test_case/test_main.py
@@ -7,10 +7,10 @@ from page.app import App
 class TestMain:
     @pytest.mark.parametrize("value1,value2", yaml.safe_load(open("../test_case/test_main.yml")))
     def test_main(self, value1, value2):
-        # app = App()
-        # app.start().main().goto_search()
-        print(value1)
-        print(value2)
+        app = App()
+        app.start().main().goto_search()
+        # print(value1)
+        # print(value2)
 
 
 if __name__ == '__main__':
